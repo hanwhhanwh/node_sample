a310d75b7186f65b264f64c49fea6951377637cd
Wonhee Han <hanwhhanwh@gmail.com>
2023-01-07 14:41:51 +0900

    함수로 리팩토링

--- mini.project/Superbin/youtube_dl_tampermonkey.js
+++ mini.project/Superbin/youtube_dl_tampermonkey.js
@@ -38,12 +38,103 @@
 (function() {
 	'use strict';
 
-	// Your code here...
-	let div = window.document.querySelector("#center");
-	try
+
+	/**
+	 * 입력한 유튜브 영상 클립의 고유 ID에 해당하는 영상을 다운로드 받습니다.
+	 * @param {string} video_id 유튜브 영상 클립의 고유 ID 문자열
+	 * @param {string} force_download 'y' = 다운로드 이력과 무관하게 다운로드, 'n' 다운로드 이력이 있으면 다운로드하지 않고 무시함
+	 */
+	let download_youtube_clip = function(video_id, force_download)
+	{
+		let strDownloadURL = `http://localhost/youtube_dl?video_id=${video_id}&force_download=${force_download}`;
+		let options = { method: 'GET' }
+
+		fetch(strDownloadURL, options)
+			.then(res => res.json())
+			.then(res => {
+				if (res.resuleCode == 200)
+				{
+					console.log(`YOUTUBE_DL download complete: ${video_id}`)
+				}
+				else
+				{
+					console.log(`youtube_dl API Error: ${res}`)
+				}
+			})
+			.catch(err => {
+				console.log(`download_youtube_clip: ${v_id} download error`)
+				console.log(err)
+			})
+	}
+
+
+	// 유튜브 다운로드 버튼에 대한 핸들러
+	let dl_button_handler = function(o)
+	{
+		let v_id = window.document.location.toString().split('/watch?v=')[1];
+		v_id = v_id.split('&')[0]
+
+		if (v_id)
+		{
+			console.log(`Download youtube video : ${v_id}`);
+			let strURL = `http://localhost:35000/youtube_dl?video_id=${v_id}`;
+			let options =
+				{
+					method: 'GET',
+					headers: { 'Content-Type': 'application/json; charset=utf-8' }
+				}
+	
+			fetch(strURL, options)
+				.then(res => res.json())
+				.then(res => {
+					let force_download = 'n'
+					if (res.resultCode == 200)
+					{
+						ret = confirm(`${v_id}: already downloaded '${1}'\nDo you want to re-download?`)
+						if (ret)
+						{
+							// Re-download same youtube clip
+							force_download = 'y'
+						}
+						else
+							return // end
+					}
+					if ( (res.resultCode == 404) || (force_download == 'y') ) // not exist download-log => new clip
+					{
+						download_youtube_clip(v_id, force_download)
+					}
+					else if ((res.resultCode != 200) && (res.resultCode != 404))
+					{ // error occured
+						console.log(`YOUTUBE_DL: Error occurred!!!`)
+						console.log(res)
+					}
+				})
+				.catch(err => {
+					console.log(`YOUTUBE_DL: ${v_id} download error`)
+					console.log(err)
+				})
+		}
+		else
+			console.log(`Error(dl_button_handler): video_id not found`)
+	}
+
+	// 유튜브 다운로드 버튼 생성하여 삽입 처리
+	let insert_dl_button = function()
 	{
 		let video_id = window.document.location.toString().split('/watch?v=')[1];
 		video_id = video_id.split('&')[0]
+		if (!video_id)
+		{
+			console.log(`Error(YOUTUBE_DL): video_id parameter not found!!!`)
+			return null
+		}
+
+		let center_div = window.document.querySelector("#center");
+		if (!center_div)
+		{
+			console.log(`Error(YOUTUBE_DL): center div not found!!!`)
+			return null
+		}
 
 		let o = document.createElement("button");
 		o.id = "youtube_dl";
@@ -52,41 +143,25 @@
 		o.setAttribute("title", "Download video");
 		o.setAttribute("style", "backgroundColor:#ff003e;");
 		o.innerHTML = "DL"
-		o.addEventListener("click", function (o) {
-			let v_id = window.document.location.toString().split('/watch?v=')[1];
-			v_id = v_id.split('&')[0]
-
-			if (v_id)
-			{
-				console.log(`Download youtube video : ${v_id}`);
-				let strURL = `http://localhost:35000/youtube_dl?video_id=${v_id}`;
-				let options = {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json; charset=utf-8'
-						, 'dataType': 'json'
-						// , 'credentials': 'same-origin'
-					}
-				}
-		
-				fetch(strURL, options)
-					.then(res => res.json())
-					.then(res => {
-						if (res.resultCode == 200) {
-							console.log(`${v_id}: added download queue.`)
-							alert(`${v_id}: added download queue.`)
-						}
-					})
-					.catch(err => {
-						console.log(`${v_id} download error`)
-						console.log(err)
-					})
-			}
-		}, !0);
-		// o.setAttribute("style", "min-height:25px; position:relative; top:1px; cursor: pointer; font: 13px Arial; background: #ff003e; color: #fff; text-transform: uppercase; display: block; padding: 10px 16px; margin: 20px 5px 10px 5px; border: 1px solid #ff0068; border-radius: 2px; font-weight:bold");
+		o.addEventListener("click", dl_button_handler)
 		o.setAttribute("onmouseover", "this.style.backgroundColor='#c10841'");
 		o.setAttribute("onmouseout", "this.style.backgroundColor='#ff003e'");
-		div.insertBefore(o, div.firstChild);
+		center_div.insertBefore(o, center_div.firstChild);
+
+		return o
+	}
+
+	let center_div = window.document.querySelector("#center");
+	console.log(center_div);
+	try
+	{
+		if (center_div)
+			insert_dl_button()
+		else
+			setTimeout(insert_dl_button, 2000)
+
+		let video_id = window.document.location.toString().split('/watch?v=')[1];
+		video_id = video_id.split('&')[0]
 
 		// 다운로드 받았던 것인지 확인합니다.
 		let strURL = `http://localhost:35000/youtube_dl?video_id=${video_id}`;
@@ -112,14 +187,14 @@
 				console.log(err)
 			})
 	}
-	catch (o) {
-		console.log("Error youtube download: ", o);
+	catch (e)
+	{
+		console.log(`Error youtube download: ${e}`);
 	}
-	console.log(div);
 
-	window.addEventListener('hashchange', function() {
-
+	window.addEventListener('hashchange', function()
+	{
 		alert(`Hash Changed : ${window.location.href}`);
-	
 	});
+
 })();
